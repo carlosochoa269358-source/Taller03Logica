19766cac7aba312c9e44abf278117bcba6aecaae
Andres Ochoa <carlosochoa269358@correo.itm.edu.co>
2026-03-08 11:01:52 -0500

    Complet

---
+++ MatrizTriangulasInferior/MatrizTriangulasInferior/Program.cs
@@ -0,0 +1,42 @@
+﻿Console.Write("Ingrese orden de la matriz: ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[n, n];
+
+// Llenar matriz con la fórmula i + j
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = i + j;
+    }
+}
+
+// Mostrar matriz completa
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write($"{matrix[i, j],4}");
+    }
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+// Mostrar triangular inferior
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        if (j <= i)
+        {
+            Console.Write($"{matrix[i, j],4}");
+        }
+        else
+        {
+            Console.Write("    ");
+        }
+    }
+    Console.WriteLine();
+}
---
+++ RelojArena/RelojArena/Program.cs
@@ -0,0 +1,58 @@
+﻿Console.Write("Ingrese orden de la matriz: ");
+int n = int.Parse(Console.ReadLine());
+
+int[,] matrix = new int[n, n];
+
+// Llenar matriz
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        matrix[i, j] = i * 2 + j;
+    }
+}
+
+// Mostrar matriz completa
+Console.WriteLine("MATRIZ COMPLETA");
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write($"{matrix[i, j],4}");
+    }
+    Console.WriteLine();
+}
+
+string[,] a = new string[n, n];
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        a[i, j] = "";
+    }
+}
+
+int x = (n / 2) + 1;
+
+for (int i = 0; i < x; i++)
+{
+    for (int j = i; j < n - i; j++)
+    {
+        a[i, j] = matrix[i, j].ToString();
+        a[n - i - 1, j] = matrix[n - i - 1, j].ToString();
+    }
+}
+
+Console.WriteLine("RELOJ DE ARENA");
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        Console.Write($"{a[i, j],4}");
+    }
+    Console.WriteLine();
+}
+
